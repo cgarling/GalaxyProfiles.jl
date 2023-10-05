5ba2237898de9dd93797c1759be6baaf19e2ba8e
cgarling <chris.t.garling@gmail.com>
2023-10-05 05:19:50 -0400

    started looking at Read 2016's CoreNFW profile
    currently hung up on their Equation 21 requiring the NFW mass, which would require a virial definition. Would rather have it in terms of fundamental parameters. This might break down into an expression with `rs` and concentration but can't do it tonight.

---
+++ src/densities/corenfw.jl
@@ -0,0 +1,172 @@
+"""
+    NFW(ρ0::Real, rs::Real)
+    NFW(ρ0::Unitful.Density, rs::Unitful.Length)
+
+Type describing the [Navarro-Frenk-White 1996 (NFW)](http://adsabs.harvard.edu/abs/1996ApJ...462..563N) density profile with scale radius `rs` and characteristic density `ρ0`. The density profile is
+
+```math
+\\rho(r) = \\frac{\\rho_0}{(r/R_s) \\, (1+r/R_s)^2}
+```
+
+The fields of `NFW` are `ρ0, rs`. The default units of `NFW` are `[ρ0] = [Msun/kpc^3], [r, rs] = [kpc], [M] = [Msun]`. This is important for quantities like [`Vcirc`](@ref), [`Vesc`](@ref), [`Φ`](@ref), [`∇Φ`](@ref), and [`∇∇Φ`](@ref) which involve the gravitational constant `G`; these will give incorrect results if the fields of `NFW` or the provided `r` are in different units.
+
+The following public methods are defined on this type:
+ - [`ρ`](@ref), [`invρ`](@ref), [`∇ρ`](@ref), [`ρmean`](@ref), [`Σ`](@ref), [`M`](@ref), [`∇M`](@ref), [`invM`](@ref), [`Mproj`](@ref), [`∇Mproj`](@ref), [`Vmax`](@ref), [`Vesc`](@ref), [`Φ`](@ref), [`∇Φ`](@ref), [`∇∇Φ`](@ref)
+"""
+struct NFW{T<:Real} <: AbstractDensity
+    ρ0::T
+    rs::T
+end
+NFW(ρ0::Real, rs::Real) = NFW(promote(ρ0,rs)...)
+
+"""
+    CoreNFW(ρ0::Real, rs::Real, rc::Real, n::Real)
+
+Type implementing the CoreNFW density profile of [Read et al. 2016](https://ui.adsabs.harvard.edu/abs/2016MNRAS.459.2573R/abstract). This is a modified [`NFW`](@ref) profile with lower central densities calibrated to reproduce the density profiles observed in their simulations. The parameters `ρ0` and `rs` are identical to those for the NFW profile. However, the CoreNFW profile makes the following alteration to the enclosed mass,
+
+```math
+\\begin{aligned}
+M_\\text{c,NFW}(<R) &= M_\\text{NFW}(<R) \\times f^n \\newline
+f^n &= \\left[ \\text{tanh} \\left( \\frac{r}{r_c} \\right) \\right]^n
+```
+
+where ``n`` and ``r_c`` are new free parameters. 
+
+"""
+struct CoreNFW{T<:Real} <: AbstractDensity
+    # We'll save the NFW object rather than ρ0 and rs so we don't have to create it
+    # every time we want to evaluate the non-cored NFW profile
+    NFW::NFW{T} 
+    rc::T
+    n::T
+end
+function CoreNFW(ρ0::Real, rs::Real, rc::Real, n::Real)
+    T = promote_type( typeof(ρ0), typeof(rs), typeof(rc), typeof(n) )
+    CoreNFW( NFW(convert(T,ρ0), convert(T,rs)), convert(T,rc), convert(T,n))
+end
+
+#### Parameters
+
+params(d::CoreNFW) = (d.NFW.ρ0,d.NFW.rs,d.rc,d.n)
+scale_radius(d::CoreNFW) = d.NFW.rs
+
+#### Convenience functions
+
+f_cnfw(r,rc) = tanh(r/rc) # Equation 17, Read 2016
+
+#### Evaluation
+
+function ρ(d::CoreNFW, r::Real)
+    ρ0,rs,rc,n = params(d)
+    x = r/rs
+    ρ0 / x / (1+x)^2
+end
+function invρ(d::NFW, x::Real)
+    ρ0, rs = params(d)
+    tmp = cbrt(2x / (2x + 27ρ0 + 3*sqrt(3ρ0*(27ρ0+4x)))) # cbrt more efficient than ^(1/3)
+    rs/3 * (tmp + inv(tmp) - 2) 
+end
+function ∇ρ(d::NFW, r::Real)
+    ρ0, rs = params(d)
+    -ρ0 * rs^3 * (3 * r * rs) / (r^2 * (r + rs)^3)
+end
+function ρmean(d::NFW, r::Real)
+    ρ0, rs = params(d)
+    3 * rs^3 * ρ0 * (-r + ( r + rs ) * log( ( r + rs ) /
+            rs ) ) / (r^3 * ( r + rs ) )
+end
+# invρmean fallback to common.jl is fine
+function Σ(d::NFW{T}, r::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0, rs = params(d)
+    x = r/rs
+    if x ≈ 1 # approx for very near 1
+        U(2 * ρ0 * rs / 3)
+    elseif x < 1
+        x2m1 = x^2 - 1
+        rs * ρ0 * 2 / x2m1 * (1 - 2 / sqrt(-x2m1) * atanh( sqrt( (1 - x) / (x + 1) ) ) )
+    else
+        x2m1 = x^2 - 1
+        rs * ρ0 * 2 / x2m1 * (1 - 2 / sqrt(x2m1) * atan( sqrt( (x - 1) / (x + 1) ) ) )
+    end
+end
+# ∇Σ
+# Σmean fallback to common.jl is fine
+# invΣ
+function M(d::NFW{T}, r::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0,rs = params(d)
+    ρ0,rs,r = promote(ρ0, rs, r)
+    4 * U(π) * rs^3 * ρ0 * NFWmu(r, rs)
+end
+function ∇M(d::NFW{T}, r::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0,rs = params(d)
+    ρ0, rs, r = promote(ρ0, rs, r)
+    4 * U(π) * r * rs * ρ0 / (1 + r/rs)^2
+end
+function invM(d::NFW{T}, x::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0,rs = params(d)
+    ρ0, rs, x = promote(ρ0, rs, x)
+    t = -1/lambertw(-exp(-1-(x/(4 * U(π) * rs^3 * ρ0))))
+    (t - 1) * rs
+end
+# Mtot
+function Mproj(d::NFW{T}, r::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0, rs = params(d)
+    ρ0, rs, r = promote(ρ0, rs, r)
+    x = r/rs
+    if x ≈ 1
+        4 * U(π) * ρ0 * rs^3 * ( x + log( x / 2 ) )
+    elseif x<1
+        return 4 * U(π) * ρ0 * rs^3 * ( acosh( 1 / x ) / sqrt( 1 - x^2 ) + log( x / 2 ) )
+    else
+        return 4 * U(π) * ρ0 * rs^3 * ( acos( 1 / x ) / sqrt( x^2 - 1 ) + log( x / 2 ) )
+    end
+end
+function ∇Mproj(d::NFW{T},r::S) where {T,S<:Real}
+    U = promote_type(T,S)
+    ρ0,rs = params(d)
+    ρ0,rs,r = promote(ρ0,rs,r)
+    x = r/rs
+    if x ≈ 1 
+        4 * U(π) * ρ0 * rs^2 * (1+inv(x)) / 6
+    elseif x<1
+        ix = inv(x)
+        # return 4 * U(π) * ρ0 * rs^2 * (ix + sqrt(ix+ix^2)/(x-1)/(1+x)^(3/2) + x*asech(x)/(1-x^2)^(3/2))
+        return 4 * U(π) * ρ0 * rs^2 * (ix + sqrt(ix+ix^2)/(x-1)/sqrt((1+x)^3) + x*asech(x)/sqrt((1-x^2)^3)) # sqrt(x^3) is much more efficient than x^(3/2), annoyingly
+    else
+        ix = inv(x)
+        # return 4 * U(π) * ρ0 * rs^2 * ( ix + ix^2/sqrt(1-ix^2)/sqrt(x^2-1) - x*acos(ix)/(x^2-1)^(3/2))
+        return 4 * U(π) * ρ0 * rs^2 * ( ix + ix^2/sqrt(1-ix^2)/sqrt(x^2-1) - x*acos(ix)/sqrt((x^2-1)^3))
+    end
+end
+# invMproj fallback to common.jl is fine
+# Vcirc fallback to common.jl is fine
+# Vesc fallback to common.jl is fine
+function Vmax(d::NFW{T}) where T
+    ρ0,rs = params(d)
+    r = T(constants.nfwvmaxpar) * rs
+    return Vcirc(d,r), r
+end
+function Φ(d::NFW{T}, r::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0, rs = params(d)
+    ρ0, rs, r = promote(ρ0, rs, r)
+    4 * U(π) * U(constants.Gvelkpc) * ρ0 * rs^3 * (log(rs) - log(r+rs)) / r
+end
+function ∇Φ(d::NFW{T}, r::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0, rs = params(d)
+    ρ0, rs, r = promote(ρ0, rs, r)
+    4 * U(π) * U(constants.Gvelkpc2) * rs^3 * ρ0 * (log(r+rs) - log(rs) - r/(r+rs)) / r^2
+end
+function ∇∇Φ(d::NFW{T}, r::S) where {T, S<:Real}
+    U = promote_type(T, S)
+    ρ0,rs = params(d)
+    ρ0, rs, r = promote(ρ0, rs, r)
+    4 * U(π) * U(constants.Gvelkpc2) * rs^3 * ρ0 * (2*log(rs) - 2*log(r+rs) + (3*r^2 + 2*rs*r)/(r+rs)^2) / r^3
+end
+# Could add kinetic energy, line-of-sight velocity dispersion, maybe a few other quantities that could be useful from python/profiles/nfw.py
