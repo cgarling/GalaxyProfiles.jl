41062d63d777be346fba6a9644e0f1c6197e4162
cgarling <chris.t.garling@gmail.com>
2024-10-27 04:54:41 -0400

    clean up nfw

--- src/densities/nfw.jl
+++ src/densities/nfw.jl
@@ -38,17 +38,17 @@ function ρ(d::NFW, r::Real)
 end
 function invρ(d::NFW, x::Real)
     ρ0, rs = params(d)
-    tmp = cbrt(2x / (2x + 27ρ0 + 3*sqrt(3ρ0*(27ρ0+4x)))) # cbrt more efficient than ^(1/3)
+    tmp = cbrt(2x / (2x + 27ρ0 + 3sqrt(3ρ0*(27ρ0+4x)))) # cbrt more efficient than ^(1/3)
     return rs/3 * (tmp + inv(tmp) - 2) 
 end
 function ∇ρ(d::NFW, r::Real)
     ρ0, rs = params(d)
-    return -ρ0 * rs^3 * (3 * r * rs) / (r^2 * (r + rs)^3)
+    return -ρ0 * rs^3 * (3r * rs) / (r^2 * (r + rs)^3)
 end
 function ρmean(d::NFW, r::Real)
     ρ0, rs = params(d)
-    return 3 * rs^3 * ρ0 * (-r + ( r + rs ) * log( ( r + rs ) /
-        rs ) ) / (r^3 * ( r + rs ) )
+    return 3rs^3 * ρ0 * (-r + (r+rs) * log((r+rs) / rs)) /
+        (r^3 * (r+rs))
 end
 # invρmean fallback to common.jl is fine
 function Σ(d::NFW{T}, r::S) where {T, S<:Real}
@@ -59,10 +59,10 @@ function Σ(d::NFW{T}, r::S) where {T, S<:Real}
         return U(2 * ρ0 * rs / 3)
     elseif x < 1
         x2m1 = x^2 - 1
-        return rs * ρ0 * 2 / x2m1 * (1 - 2 / sqrt(-x2m1) * atanh( sqrt( (1 - x) / (x + 1) ) ) )
+        return rs * ρ0 * 2 / x2m1 * (1 - 2 / sqrt(-x2m1) * atanh( sqrt((1-x) / (x+1)) ) )
     else
         x2m1 = x^2 - 1
-        return rs * ρ0 * 2 / x2m1 * (1 - 2 / sqrt(x2m1) * atan( sqrt( (x - 1) / (x + 1) ) ) )
+        return rs * ρ0 * 2 / x2m1 * (1 - 2 / sqrt(x2m1) * atan( sqrt((x - 1) / (x + 1)) ) )
     end
 end
 # ∇Σ
@@ -98,13 +98,13 @@ function ∇Mproj(d::NFW, r::Real)
     x = r/rs
     invx = inv(x)
     if x ≈ 1 
-        ρ0 * rs^2 * 4 * π * (1+invx) / 6
+        return ρ0 * rs^2 * 4 * π * (1+invx) / 6
     elseif x < 1
         # return 4 * U(π) * ρ0 * rs^2 * (ix + sqrt(ix+ix^2)/(x-1)/(1+x)^(3/2) + x*asech(x)/(1-x^2)^(3/2))
         return ρ0 * rs^2 * 4 * π * (invx + sqrt(invx+invx^2)/(x-1)/sqrt((1+x)^3) + x*asech(x)/sqrt((1-x^2)^3)) # sqrt(x^3) is much more efficient than x^(3/2), annoyingly
     else
         # return 4 * U(π) * ρ0 * rs^2 * ( ix + ix^2/sqrt(1-ix^2)/sqrt(x^2-1) - x*acos(ix)/(x^2-1)^(3/2))
-        return ρ0 * rs^2 * 4 * π * ( invx + invx^2/sqrt(1-invx^2)/sqrt(x^2-1) - x*acos(invx)/sqrt((x^2-1)^3))
+        return ρ0 * rs^2 * 4 * π * (invx + invx^2/sqrt(1-invx^2)/sqrt(x^2-1) - x*acos(invx)/sqrt((x^2-1)^3))
     end
 end
 # invMproj fallback to common.jl is fine
@@ -116,7 +116,7 @@ function Vmax(d::NFW{T}) where T
     return Vcirc(d,r), r
 end
 # Roughly 2x faster than generic fallback
-function σr(d::NFW{T}, r::S, β::S) where {T <: Real, S <: Real}
+function σr(d::NFW{T}, r::S, β::S) where {T, S <: Real}
     U = promote_type(T, S)
     ρ0, rs = params(d)
     x = r / rs
