000c61cb4cbbda6ffee169a2797425a6778cfdf1
cgarling <chris.t.garling@gmail.com>
2024-11-13 21:50:22 -0500

    Revise `ExponentialDisk` code

--- src/surface_densities/exponential_disk.jl
+++ src/surface_densities/exponential_disk.jl
@@ -64,51 +64,42 @@ scale_radius(d::ExponentialDisk) = d.rs
 
 #### Evaluation
 function Σ(d::ExponentialDisk, r::Real) 
-    Σ0,rs = params(d)
-    Σ0 * exp(-r/rs)
+    Σ0, rs = params(d)
+    return Σ0 * exp(-r/rs)
 end
 function ∇Σ(d::ExponentialDisk, r::Real)
-    Σ0,rs=params(d)
-    # 2π * r * Σ0 * exp(-r/rs)
-    - Σ0 * exp(-r/rs) / rs
+    Σ0, rs = params(d)
+    return -Σ0 * exp(-r/rs) / rs
 end
 # Σmean
 function invΣ(d::ExponentialDisk, x::Real)
-    Σ0,rs = params(d)
+    Σ0, rs = params(d)
     x<=0 ? throw(DomainError(x, "x must be greater 0")) : rs * log(Σ0/x)
 end
-function Mproj(d::ExponentialDisk{T}, r::S) where {T,S<:Real}
-    isinf(r) && (return Mtot(d))
-    U = promote_type(T, S)
-    Σ0, rs=params(d)
-    Σ0, rs, r = promote(Σ0, rs, r)
-    ee = exp(-r/rs)
-    2 * U(π) * Σ0 * rs^2 * (1 - ee*(r/rs + 1))
+function Mproj(d::ExponentialDisk, r::Real)
+    Σ0, rs = params(d)
+    return isinf(r) ? Mtot(d) : Σ0 * rs^2 * (1 - exp(-r/rs)*(r/rs + 1)) * 2 * π
 end
-function ∇Mproj(d::ExponentialDisk{T}, r::S) where {T,S<:Real}
-    U = promote_type(T, S)
-    Σ0, rs=params(d)
-    Σ0, rs, r = promote(Σ0, rs, r)
-    2 * U(π) * r * Σ0 * exp(-r/rs) 
+function ∇Mproj(d::ExponentialDisk, r::Real)
+    Σ0, rs = params(d)
+    return r * Σ0 * exp(-r/rs) * 2 * π
 end
 function invMproj(d::ExponentialDisk, x::Real)
-    Σ0, rs=params(d)
-    # total_mass = 2π * Σ0 * rs^2 
-    # -rs * (1 + lambertw( ((x/total_mass) - 1) / ℯ , -1) )
-    -rs * (1 + lambertw( ((x/Mtot(d)) - 1) / ℯ , -1) )
+    Σ0, rs = params(d)
+    return -rs * (1 + lambertw( ((x/Mtot(d)) - 1) / ℯ , -1) )
 end
-function Mtot(d::ExponentialDisk{T}) where T
-    Σ0, rs=params(d)
-    2 * T(π) * Σ0 * rs^2
+function Mtot(d::ExponentialDisk)
+    Σ0, rs = params(d)
+    return Σ0 * rs^2 * 2 * π
 end
 function ccdf2D(d::ExponentialDisk, r::Real)
-    Σ0, rs=params(d)
+    Σ0, rs = params(d)
     ee = exp(-r/rs)
-    r*ee/rs + ee
+    return r*ee/rs + ee
 end
 cdf2D(d::ExponentialDisk, r::Real) = 1 - ccdf2D(d, r)
 function cquantile2D(d::ExponentialDisk, x::Real)
-    Σ0, rs=params(d)
-    -rs * (1+lambertw(-x/ℯ,-1))
+    Σ0, rs = params(d)
+    return -rs * (1+lambertw(-x/ℯ,-1))
 end
-quantile2D(d::ExponentialDisk, x::Real) = cquantile2D(d,1-x)
+quantile2D(d::ExponentialDisk, x::Real) = cquantile2D(d, 1-x)
