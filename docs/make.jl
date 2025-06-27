444336d8f948621f31aa7b2b1a7c70cb1c55dc73
cgarling <chris.t.garling@gmail.com>
2025-06-26 21:17:13 -0400

    activate `deploy docs`

--- docs/make.jl
+++ docs/make.jl
@@ -12,15 +12,15 @@ makedocs(
     format = Documenter.HTML(;prettyurls = get(ENV, "CI", nothing) == "true"),
     authors = "Chris Garling",
     pages = ["index.md","types.md","methods.md","guide.md","units.md","docindex.md"],
-    push_preview = true,
     doctest = true,
     linkcheck = true,
     # Do not error if we are missing a docstring in the module or if an external link is invalid
     warnonly = [:missing_docs, :linkcheck]    
 )
 
-# deploydocs(;
-#     repo = "github.com/cgarling/GalaxyProfiles.jl.git",
-#     versions = ["stable" => "v^", "v#.#"],
-#     push_preview=true,
-# )
+deploydocs(;
+    repo = "github.com/cgarling/GalaxyProfiles.jl.git",
+    versions = ["stable" => "v^", "v#.#"],
+    devbranch="main",
+    push_preview=true,
+)
