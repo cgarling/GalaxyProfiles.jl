e7e20524f996aed1bf623ea0c33b5099a6c47e51
cgarling <chris.t.garling@gmail.com>
2026-02-04 18:23:55 -0500

    Revise `sample3D_r`

--- src/generic_rand.jl
+++ src/generic_rand.jl
@@ -101,7 +101,7 @@ function sample2D(rng::AbstractRNG, d::AbstractMassProfile, center::AbstractSkyC
 end
 sample2D(d::AbstractMassProfile, center::AbstractSkyCoords, distance::Real, dims::Dims) =
     sample2D(default_rng(), d, center, distance, dims...)
-    
+
 #### Now do the 3D samplers
 # Mutating forms
 
@@ -110,8 +110,11 @@ sample2D(d::AbstractMassProfile, center::AbstractSkyCoords, distance::Real, dims
 
 Draw multiple samples of the radius following the density profile of `d` and write them to `x` in place. By default, falls back to [`quantile3D.(d, rand!(rng, x))`](@ref quantile3D). See also [`sample3D_r`](@ref).
 """
-sample3D_r!(rng::AbstractRNG, d::AbstractDensity, x::AbstractArray{<:Real}) = 
-    ( rand!(rng, x); x .= quantile3D.(d, x) )
+function sample3D_r!(rng::AbstractRNG, d::AbstractDensity, x::AbstractArray{<:Real})
+    rand!(rng, x)
+    x .= quantile3D.(d, x)
+    return x
+end
 sample3D_r!(d::AbstractDensity, x::AbstractArray{<:Real}) = sample3D_r!(default_rng(), d, x)
 
 # Non-mutating forms
@@ -120,9 +123,12 @@ sample3D_r!(d::AbstractDensity, x::AbstractArray{<:Real}) = sample3D_r!(default_
 
 Sample random points following the density profile of `d` with shape `[dims...]`, which can either be a `Vararg{Int,N}` (e.g., `sample3D_r(rng, d, 2, 2)` to generate a 2x2 Matrix) or a `Dims{N}`, which is an `NTuple` of `N` `Int`s (e.g., `sample3D_r(rng, d, (2,2))`. By default, falls back to [`quantile3D.(d, rand(rng [, dims...]))`](@ref quantile3D). See also [`sample3D_r!`](@ref).
 """
-sample3D_r(rng::AbstractRNG, d::AbstractDensity, dims::Vararg{Int, N}) where N =
-    ( x = rand(rng, dims...); x .= quantile3D.(d, x) )
-sample3D_r(d::AbstractDensity, dims::Vararg{Int, N}) where N =
+function sample3D_r(rng::AbstractRNG, d::AbstractDensity, dims::Integer...)
+    x = rand(rng, dims...)
+    x .= quantile3D.(d, x)
+    return x
+end
+sample3D_r(d::AbstractDensity, dims::Integer...) = 
     sample3D_r(default_rng(), d, dims...)
 sample3D_r(rng::AbstractRNG, d::AbstractDensity, dims::Dims) = sample3D_r(rng, d, dims...)
 sample3D_r(d::AbstractDensity, dims::Dims) = sample3D_r(default_rng(), d, dims)
